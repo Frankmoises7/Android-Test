ffdb0108ad6a687b09a4087a5e4310fd37d93331
Frank Islas <frank.islas.zuniga@ciisa.cl>
2022-06-06 12:36:02 -0400

    Agregado de botones crud

--- app/src/main/java/com/example/eva_1_app_movil/ClientItemActivity.kt
+++ app/src/main/java/com/example/eva_1_app_movil/ClientItemActivity.kt
@@ -4,6 +4,7 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.Button
 import android.widget.TextView
+import android.widget.Toast
 import com.example.eva_1_app_movil.models.Client
 
 class ClientItemActivity : AppCompatActivity() {
@@ -12,9 +13,18 @@ class ClientItemActivity : AppCompatActivity() {
         setContentView(R.layout.activity_client_item)
 
         val client = intent.getSerializableExtra("client") as Client
-
         val tvTitle = findViewById<TextView>(R.id.client_item_activity_tv_title)
+        val btnUpdate = findViewById<Button>(R.id.client_item_activity_btn_update)
+        val btnDelete = findViewById<Button>(R.id.client_item_activity_btn_delete)
 
         tvTitle.text = "ID ${client.id} - ${client.title}"
+
+        btnUpdate.setOnClickListener {
+            Toast.makeText(this, "Update disponible Proximamente", Toast.LENGTH_SHORT).show()
+        }
+
+        btnDelete.setOnClickListener {
+            Toast.makeText(this, "Delete disponible Proximamente", Toast.LENGTH_SHORT).show()
+        }
     }
 }
