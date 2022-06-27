de177c13c928fd5edd2680b58c587e5191cbee18
Alejandro Saez <jaaneo@gmail.com>
2022-06-27 01:39:07 -0400

    Se añade el BCrypt

--- app/src/main/java/com/example/eva_1_app_movil/dao/UserClientDAO.kt
+++ app/src/main/java/com/example/eva_1_app_movil/dao/UserClientDAO.kt
@@ -1,6 +1,7 @@
 package com.example.eva_1_app_movil.dao
 
 import androidx.room.Dao
+import androidx.room.Delete
 import androidx.room.Insert
 import androidx.room.Query
 import com.example.eva_1_app_movil.models.UserClientEntity
@@ -8,8 +9,11 @@ import com.example.eva_1_app_movil.models.UserClientEntity
 @Dao
 interface UserClientDAO {
     @Query("SELECT * FROM usersClients WHERE email = :email LIMIT 1")
-    fun findByEmail(email: String): UserClientEntity
+    fun findByEmail(email: String): UserClientEntity?
 
     @Insert
     fun insert(user: UserClientEntity)
+
+    @Delete
+    fun delete(user: UserClientEntity)
 }
